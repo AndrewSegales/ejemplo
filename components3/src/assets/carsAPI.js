17601b41b25f84db5abb8df8efa01c3a40637f9a
Andrew Segales <andysejas444@gmail.com>
2023-10-05 20:15:45 -0400

    componentes

---
+++ components3/src/assets/carsAPI.js
@@ -0,0 +1,21 @@
+
+export const cars = [
+  {
+    title: "Sedans",
+    description:"Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.",
+    imgUrl: "/icon-sedans.svg",
+    theme: "orange",
+  },
+  {
+    title: "Suvs",
+    description:"Take an SUV for its spacious interior, power and veraitily. Perfect for your next faminily vacation and off-road adventures.",
+    imgUrl: "/icon-suvs.svg",
+    theme: "green",
+  },
+  {
+    title: "Luxury",
+    description:"Cruise in the best car brands without the bloaoated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.",
+    imgUrl: "/icon-luxury.svg",
+    theme: "dark-green",
+  }
+]
